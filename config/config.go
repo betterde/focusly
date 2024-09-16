f587b62c04d803a44e0803544108b1fa9cb534cb
George King <george@betterde.com>
2024-09-17 07:31:21 +0800

    minor: optimize config

--- config/config.go
+++ config/config.go
@@ -1,38 +1,61 @@
 package config
 
 import (
+	"errors"
+	"fmt"
 	"github.com/betterde/focusly/internal/journal"
 	"github.com/spf13/viper"
 	"os"
 	"strings"
 )
 
-var Conf *Config
+var (
+	keys = []string{
+		"ENV",
+		"HTTP.LISTEN",
+		"HTTP.TLSKEY",
+		"HTTP.TLSCERT",
+		"LOGGING.LEVEL",
+		"DATABASE.URL",
+		"DATABASE.DATABASE",
+		"DATABASE.USERNAME",
+		"DATABASE.PASSWORD",
+		"DATABASE.NAMESPACE",
+	}
+	Conf *Config
+)
 
 type Config struct {
 	// Add config item to here
-	Env      string   `yaml:"env"`
-	HTTP     HTTP     `yaml:"http"`
-	Logging  Logging  `yaml:"logging"`
-	Database Database `yaml:"database"`
+	Env      string   `yaml:"env" mapstructure:"ENV"`
+	HTTP     HTTP     `yaml:"http" mapstructure:"HTTP"`
+	GRPC     GRPC     `yaml:"grpc" mapstructure:"GRPC"`
+	Logging  Logging  `yaml:"logging" mapstructure:"LOGGING"`
+	Database Database `yaml:"database" mapstructure:"DATABASE"`
 }
 
 type HTTP struct {
-	Listen  string `yaml:"listen"`
-	TLSKey  string `yaml:"tlsKey"`
-	TLSCert string `yaml:"tlsCert"`
+	Listen  string `yaml:"listen" mapstructure:"LISTEN"`
+	TLSKey  string `yaml:"tlsKey" mapstructure:"TLSKEY"`
+	TLSCert string `yaml:"tlsCert" mapstructure:"TLSCERT"`
+}
+
+type GRPC struct {
+	Listen  string `yaml:"listen" mapstructure:"LISTEN"`
+	TLSKey  string `yaml:"tlsKey" mapstructure:"TLSKEY"`
+	TLSCert string `yaml:"tlsCert" mapstructure:"TLSCERT"`
 }
 
 type Logging struct {
-	Level string `yaml:"level"`
+	Level string `yaml:"level" mapstructure:"LEVEL"`
 }
 
 type Database struct {
-	URL       string `yaml:"url"`
-	Database  string `yaml:"database"`
-	Username  string `yaml:"username"`
-	Password  string `yaml:"password"`
-	Namespace string `yaml:"namespace"`
+	URL       string `yaml:"url" mapstructure:"URL"`
+	Database  string `yaml:"database" mapstructure:"DATABASE"`
+	Username  string `yaml:"username" mapstructure:"USERNAME"`
+	Password  string `yaml:"password" mapstructure:"PASSWORD"`
+	Namespace string `yaml:"namespace" mapstructure:"NAMESPACE"`
 }
 
 func Parse(file string, envPrefix string) {
@@ -41,16 +64,24 @@ func Parse(file string, envPrefix string) {
 	} else {
 		viper.AddConfigPath(".")
 		viper.SetConfigName(".focusly")
+		viper.AddConfigPath("/etc/focusly")
 	}
 
+	replacer := strings.NewReplacer(".", "_")
+
 	// read in environment variables that match
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.SetEnvKeyReplacer(replacer)
 	viper.SetEnvPrefix(envPrefix)
 
+	var notFoundError viper.ConfigFileNotFoundError
+
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err != nil {
-		journal.Logger.Errorf("Failed to read configuration file: %s", err)
-		os.Exit(1)
+	if err := viper.ReadInConfig(); err != nil && errors.As(err, &notFoundError) {
+		for _, key := range keys {
+			if err := viper.BindEnv(key, fmt.Sprintf("%s_%s", envPrefix, replacer.Replace(key))); err != nil {
+				journal.Logger.Error(err)
+			}
+		}
 	}
 
 	// read in environment variables that match
